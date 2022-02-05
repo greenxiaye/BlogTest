4f295d839e9f3f18e5eefe8dc7e1db2c919d6621
greenxiaye <greenxiaye@163.com>
2022-02-06 04:49:18 +0800

    增加article路由

--- blog/server.js
+++ blog/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
+
+app.use("/articles", articleRouter)
 
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
     res.render('index')
-}) 
+})
 
 app.listen(5000)
