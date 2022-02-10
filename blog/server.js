7bf9f3b832c9122b6477525fee02de66dee1432b
greenxiaye <greenxiaye@163.com>
2022-02-10 17:41:00 +0800

    添加bootstrap样式

--- blog/server.js
+++ blog/server.js
@@ -7,7 +7,16 @@ app.use("/articles", articleRouter)
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-    res.render('index', { text: 'articles' })
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test Article Description'
+    }, {
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        description: 'Test Article Description 2'
+    }]
+    res.render('index', { articles: articles })
 })
 
 app.listen(5000)
